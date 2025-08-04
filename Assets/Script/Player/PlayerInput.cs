b032595abf7eb0e5ed9b7777c05e72cdaca4d88a
Yoon <panopticon0847@gmail.com>
2025-08-04 18:29:58 +0900

    feat : playerInput 수정
    fire, reload 버튼 추가

--- Assets/Script/Player/PlayerInput.cs
+++ Assets/Script/Player/PlayerInput.cs
@@ -4,11 +4,17 @@ public class PlayerInput : MonoBehaviour
 {
     public string moveAxisName = "Vertical"; //앞뒤 움직임을 위한 입력축 이름
     public string rotateAxisName = "Horizontal"; //좌우 움직임을 위한 입력축 이름       
+    public string fireButtonName = "Fire1";
+    public string reloadButtonName = "Reload";
+
+
     public VariableJoystick joystick;
     public FixedJoystick rotateJoystick;
 
     public float move { get; private set; } //감지된 움직임 입력값
     public float rotate{ get; private set; } //감지된 회전 입력값
+    public bool fire { get; private set; }
+    public bool reload { get; private set; }
 
 
     void Update()
@@ -20,6 +26,8 @@ public class PlayerInput : MonoBehaviour
 
         move = joystick.Vertical;
         rotate = rotateJoystick.Horizontal;
+        fire = Input.GetButton(fireButtonName);
+        reload = Input.GetButtonDown(reloadButtonName);
 
     }
 }
