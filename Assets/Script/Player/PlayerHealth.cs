04bfb53bcd4f464374c98c71fb1757de99312cdf
Yoon <panopticon0847@gmail.com>
2025-08-07 13:55:23 +0900

    feat : 플레이어 체력 관련 스크립트 작성

--- Assets/Script/Player/PlayerHealth.cs
+++ Assets/Script/Player/PlayerHealth.cs
@@ -1,16 +1,77 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : LivingEntity
 {
-    
-    void Start()
+    public Slider healthSlider;
+
+    public AudioClip deathClip;
+    public AudioClip hitClip;
+    public AudioClip itemPickupClip;
+    public AudioClip HealingClip;
+
+    AudioSource audioSource;
+    Animator animator;
+
+    PlayerMove playerMove;
+    PlayerShooter shooter;
+
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        playerMove = GetComponent<PlayerMove>();
+        shooter = GetComponent<PlayerShooter>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnEnable()
     {
-        
+        base.OnEnable();
+
+        healthSlider.gameObject.SetActive(true);
+        healthSlider.maxValue = DefaultHealth;
+
+        healthSlider.value = health;
+
+        playerMove.enabled = true;
+        shooter.enabled = true;
+    }
+
+    public override void RestoreHealth(float newHealth)
+    {
+        base.RestoreHealth(newHealth);
+        healthSlider.value = health;
+    }
+
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
+        if (!dead)
+        {
+            audioSource.PlayOneShot(hitClip);
+        }
+        base.OnDamage(damage, hitPoint, hitNormal);
+        healthSlider.value = health;
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        healthSlider.gameObject.SetActive(false);
+
+        audioSource.PlayOneShot(deathClip);
+        animator.SetTrigger("Die");
+
+        playerMove.enabled=false;
+        shooter.enabled=false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!dead)
+        {
+            
+        }
     }
 }
